0e82436254934b41773a93cea9376250c83dcd66
Victor Ong <waikitong168@gmail.com>
2026-04-11 03:18:10 +0700

    getting ready for

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -11,8 +11,6 @@ export interface MenuItem {
 }
 
 export const headerMenu: MenuItem[] = [
-	//{ name: 'Theme Info', link: '/theme-info' },
-	//{ name: 'Our Team', link: '/team' },
 	{ name: 'Company', link: '/company' },
 	{ name: 'My Certificates', link: '/cert' },
 	{ name: 'Projects', link: '/projects' },
@@ -24,7 +22,3 @@ export const footerMenu: MenuItem[] = [
 	{ name: 'My Projects', link: '/projects' },
 ];
 
-//export const legalMenu: MenuItem[] = [
-//	{ name: 'Privacy Policy', link: '/legal/privacy-policy' },
-//	{ name: 'Terms of Service', link: '/legal/terms-of-service' },
-//];
